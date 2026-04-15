77dead15df40d4c87b2e4c246ed0edfee7d76b6c
Rodrigo91218 <pereira97rodrigo@gmail.com>
2026-04-14 21:39:30 -0300

    Agregar archivos de proyecto.

---
+++ Actividad tienda virtual+repaso/Producto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_tienda_virtual_repaso
+{
+    internal class Producto
+    {
+    }
+}
---
+++ Actividad tienda virtual+repaso/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_tienda_virtual_repaso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
